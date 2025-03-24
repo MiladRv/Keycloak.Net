c5e56b43a444225ea2fd23d2dbd4c36861c61ed6
Milad Rivandi <lvlilad.l2ivandi@gmail.com>
2025-03-24 20:47:12 +0330

    feature: configuration `Polly` (#5)
    * feat: implement contracts
    
    * feat: implement KeyCloakAdminClient.cs
    
    * Create release.yml
    
    * Update release.yml
    
    * ref: add .sln file
    
    * add .sln file (#2)
    
    * first version
    
    * feat: implement contracts
    
    * feat: implement KeyCloakAdminClient.cs
    
    * Create release.yml
    
    * Update release.yml
    
    ---------
    
    Co-authored-by: Milad-Pc <m.rivandi@sgity.ir>
    
    * Create release.yml
    
    ---------
    
    Co-authored-by: Milad-Pc <m.rivandi@sgity.ir>
    
    * ref: add package version
    
    * ref: update `.gitignore`
    
    * ref: remove useless files
    
    * chore: move `Polly` configuration to `KeyCloakAdminClient.cs`
    
    * chore: upgrade package version
    
    ---------
    
    Co-authored-by: Milad-Pc <m.rivandi@sgity.ir>

--- Keycloak.Net.Sdk/Contracts/KeycloakConfiguration.cs
+++ Keycloak.Net.Sdk/Contracts/KeycloakConfiguration.cs
@@ -1,12 +1,14 @@
 ﻿namespace Keycloak.Net.Sdk.Contracts;
 
-public record KeycloakConfiguration
+public sealed record KeycloakConfiguration
 {
-    public string ServerUrl { get; set; }
-    public string RealmName { get; set; }
-    public string ClientUuid { get; set; }
-    public string ClientId { get; set; }
-    public string ClientSecret { get; set; }
-    public string AdminUsername { get; set; }
-    public string AdminPassword { get; set; }
+    public string ServerUrl { get; init; }
+    public string RealmName { get; init; }
+    public string ClientUuid { get; init; }
+    public string ClientId { get; init; }
+    public string ClientSecret { get; init; }
+    public string AdminUsername { get; init; }
+    public string AdminPassword { get; init; }
+    public ushort DelayBetweenRetryRequestsInSeconds { get; init; } = 2;
+    public ushort NumberOfRetries { get; init; } = 3;
 }
